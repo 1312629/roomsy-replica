6adacdc846fe345d049fde06333c89daad552d39
Tuan Nguyen <nmtuan.dev@gmail.com>
2016-10-09 07:27:28 +0700

    Update some front-end stuff

--- product/public/src/js/app-controllers/response.controller.js
+++
@@ -1,9 +0,0 @@
-appControllers.controller('responseController', ['$scope',
-    function ($scope) {
-        $scope.response = '';
-        $scope.$on('response', function(events, value) {
-            $scope.response = value;
-            $scope.$apply();
-        })
-    
-    }]);
